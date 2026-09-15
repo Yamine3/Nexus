71cc2d9095eee55f692354cf2f4e03df7dd3b564
Yamine3 <yamine.aissani.pro@gmail.com>
2026-09-15 11:45:11 +0200

    First push
    First push with git ignore

---
+++ nexus/nexus/nexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexusApp.swift
+//  nexus
+//
+//  Created by Apprenant 109 on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct nexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
